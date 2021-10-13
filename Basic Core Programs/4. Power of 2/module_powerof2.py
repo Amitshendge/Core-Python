48a7f62de1b66f36cfb9fd66ac5ced5521baa912
Amitshendge <Amitshendgepro@gmail.com>
2021-10-13 19:56:19 +0530

    [Amit] Add : Power of 2 list till given number program

---
+++ Basic Core Programs/4. Power of 2/module_powerof2.py
@@ -0,0 +1,15 @@
+'''
+@Author: Amit Shendge
+@Date: 13-10-2021 08:40AM
+@Last Modified by: Amit Shendge
+@Last Modified time: 13-10-2021 08:40AM
+@Title : Leap Year or Not a Leap Year
+'''
+
+var_number = int(input('Enter the Number : '))
+var_list = []
+
+for i in range(1,var_number+1):
+    var_list.append(int(i**2))
+
+print(var_list)

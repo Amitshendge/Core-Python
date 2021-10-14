0af8ac95958a9d3531337def1cf1a55b730790be
Amitshendge <Amitshendgepro@gmail.com>
2021-10-14 11:23:49 +0530

    [Amit] Add : Distance between origin and given point

---
+++ Functional Programing/3. Distance/module_distance.py
@@ -0,0 +1,44 @@
+'''
+@Author: Amit Shendge
+@Date: 14-10-2021 11:11AM
+@Last Modified by: Amit Shendge
+@Last Modified time: 14-10-2021 11:11AM
+@Title : Distance between origin and given point
+'''
+import math
+
+def function_intInput(string):
+    """
+Description:
+    Function is used to take user input with input validation chaecks for valueError
+Parameter:
+      string which should be shown in terminal
+Return:
+       user Input
+"""
+    while True:
+        try:
+            return int(input(string))
+        except ValueError:
+            print('Plz enter valid integer input')
+            continue
+
+
+def function_distance(x,y):
+    """
+Description:
+    Function is used to find Distance between origin and given point
+Parameter:
+      x and y User inputs
+Return:
+       distance between given point to origin
+"""
+    return (math.sqrt((x*x) + (y*y)))
+
+
+a='Enter x : '
+x = function_intInput(a)
+a='Enter y : '
+y = function_intInput(a)
+
+print(function_distance(x,y))

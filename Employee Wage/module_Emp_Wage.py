24d43c15f448a277a93dfeffadd01484131a6180
Amitshendge <Amitshendgepro@gmail.com>
2021-10-19 23:07:36 +0530

    [Amit] add : Employee wage with random function to set present or absent

--- Employee Wage/module_Emp_Wage.py
+++ Employee Wage/module_Emp_Wage.py
@@ -1,2 +1,19 @@
+import random
+
+
+class Employee:
+    def __init__(self) -> None:
+        self.present = False
+
+    def emp_present(self):
+        randomNum = random.random()
+        if randomNum<0.5:
+            self.present = True
+        else:
+            self.present = False
+
+
 if __name__ == '__main__':
-    print('Welcome to Employee Wage Computation Program')
+    emp1 = Employee()
+    emp1.emp_present()
+    print(emp1.present)

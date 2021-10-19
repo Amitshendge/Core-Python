082ff1440b8e8387cedcf3d11de14af713ad2bbf
Amitshendge <Amitshendgepro@gmail.com>
2021-10-19 23:32:36 +0530

    [Amit] add : Employee wage with daily wages calculation method

--- Employee Wage/module_Emp_Wage.py
+++ Employee Wage/module_Emp_Wage.py
@@ -1,19 +1,53 @@
-import random
+'''
+@Author: Amit Shendge
+@Date: 19-10-2021 11:17PM
+@Last Modified by: Amit Shendge
+@Last Modified time: 19-10-2021 11:17PM
+@Title : Employee wage with daily wages
+'''
 
+import random
 
 class Employee:
     def __init__(self) -> None:
         self.present = False
 
     def emp_present(self):
+        """
+        Description:
+            Function is used generate random to assign present or absent to employee object
+        Parameter:
+            self object is passed
+        Return:
+            doesn't returns anything but sets present status of employee
+        """
         randomNum = random.random()
         if randomNum<0.5:
             self.present = True
+            print('Employee is Present')
         else:
             self.present = False
+            print('Employee is Absent')
 
+    def daily_emp_wage(self):
+        """
+        Description:
+            Function is used generate daily wages made by employee
+        Parameter:
+            self object is passed
+        Return:
+            doesn't returns anything but prints daily wages
+        """
+        static_var_WagePerHr = 20
+        static_var_FullDayHr = 8
+        if self.present == False:
+            print('Employee wage of the day will be Zero because he/she is Absent')
+        else:
+            print('Employee wage of the day will be : ',(static_var_WagePerHr * static_var_FullDayHr),'$')
+
+    
 
 if __name__ == '__main__':
     emp1 = Employee()
     emp1.emp_present()
-    print(emp1.present)
+    emp1.daily_emp_wage()
